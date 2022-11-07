efab3e17f26fc0725a8fde669ae6f817859abdb1
estilbee <odaouphars@gmail.com>
2022-11-07 13:22:57 -0500

    added first test

--- Develop/index.js
+++ Develop/index.js
@@ -1,22 +1,32 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require ('fs')
 
 // TODO: Create an array of questions for user input
 const questions = [
-    inquirer.prompt([
+   
         {
             type:"input",
             message: "What is the title of your project?",
             name: "projectTitle"
-        }
-    ])
+        },
+        {
+            type:"input",
+            message: "What is the project description?",
+            name: "description"
+        },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => {
+        fs.writeFileSync("testreadme.md", generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
---
+++ Develop/testreadme.md
@@ -0,0 +1,5 @@
+# testing
+   ## Description
+   baba
+
+
